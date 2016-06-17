a2b5616f92918bfa178825499ffa32a29c1c5c0f
ansteh <stehle.andre@googlemail.com>
2016-06-18 01:31:38 +0200

    initial

---
+++ demo/words.js
@@ -0,0 +1,89 @@
+'use strict';
+const convnetjs = require('convnetjs');
+const loadJsonFile = require('load-json-file');
+const path = require('path');
+const _ = require('lodash');
+
+const getTrainer = () => {
+  let layer_defs = [];
+  layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:2});
+  layer_defs.push({type:'fc', num_neurons:20, activation:'relu'});
+  layer_defs.push({type:'fc', num_neurons:20, activation:'relu'});
+  layer_defs.push({type:'softmax', num_classes:2});
+
+  let net = new convnetjs.Net();
+  net.makeLayers(layer_defs);
+
+  let trainer = new convnetjs.Trainer(net, {learning_rate:0.01, l2_decay:0.001});
+
+  return trainer;
+};
+
+const parseToNumber = (value) => {
+  if(_.isNumber(value)) {
+    return value;
+  }
+  return 0;
+};
+
+var prepare = (row) => {
+  let input = [];
+  input.push(parseToNumber(row.bid));
+  input.push(parseToNumber(row.competition));
+
+  let output = (row.avgmonthlysearch > 1000) ? 1 : 0;
+
+  return {
+    input: input,
+    output: output
+  };
+};
+
+const splitSets = (data, percent) => {
+  data = _.shuffle(data);
+
+  let limit = data.length*percent;
+  let trainingsSet = _.slice(data, 0, limit);
+  let testSet = _.slice(data, limit);
+
+  return {
+    training: trainingsSet,
+    testing: testSet
+  };
+};
+
+loadJsonFile(path.resolve(__dirname, 'keywords.json')).then(json => {
+  //let trainer = getTrainer();
+  let layer_defs = [];
+  layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:2});
+  layer_defs.push({type:'fc', num_neurons:20, activation:'relu'});
+  layer_defs.push({type:'fc', num_neurons:20, activation:'relu'});
+  layer_defs.push({type:'softmax', num_classes:2});
+
+  let net = new convnetjs.Net();
+  net.makeLayers(layer_defs);
+
+  let trainer = new convnetjs.Trainer(net, {learning_rate:0.01, l2_decay:0.001});
+
+  let sets = splitSets(json, 0.8);
+	console.log(json.length, json[0], _.last(json), prepare(_.first(json)));
+
+  _.forEach(sets.training, (meta) => {
+    let options = prepare(meta);
+    trainer.train(new convnetjs.Vol(options.input), options.output);
+  });
+
+  var rightCount = 0;
+
+  _.forEach(sets.testing, (meta) => {
+    let options = prepare(meta);
+    //trainer.train(new convnetjs.Vol(options.input), options.output);
+    var x = new convnetjs.Vol(options.input);
+    var probability_volume2 = net.forward(x);
+    console.log('probability that x is class 0: ' + probability_volume2.w[0]);
+    let forecast = probability_volume2.w[0] > 0.5 ? 0 : 1;
+    if(forecast === options.output) rightCount += 1;
+  });
+  console.log(rightCount, sets.testing.length, rightCount/sets.testing.length );
+	//=> {foo: true}
+}).catch(console.log);
---
+++ index.js
@@ -0,0 +1,1 @@
+require('./demo/words.js');
